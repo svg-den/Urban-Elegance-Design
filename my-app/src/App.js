3b6ecc40b7464b78cb79d42775b85fe9e3ed41e3
svg-den <sundaywanjiru@gmail.com>
2024-08-19 21:13:16 +0300

    Homepage changes done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-// import Header from './components/Header';
+import './App.css';
 import Footer from './components/Footer';
 import Home from './components/Home';
 import About from './components/About';
