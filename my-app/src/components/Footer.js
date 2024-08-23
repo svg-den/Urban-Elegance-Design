34640f6f05b7d852cae191c311d86dc8f04510fd
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-08-23 14:54:44 +0300

    SocialLinks

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SocialLinks from './SocialLinks';
 
 function Footer() {
     return (
@@ -7,12 +8,12 @@ function Footer() {
                 <h2 className='text-2xl font-semibold mb-2'>Address</h2>
                 <p>Raila Odinga Ln, Nairobi, KE, 00100</p>
                 <h2 className='text-2xl font-semibold mt-4 mb-2'>Phone Number</h2>
-                <p>113-456-7890</p>
+                <p>(+254) 707 825 189</p>
                 <h2 className='text-2xl font-semibold mt-4 mb-2'>Email</h2>
                 <p>info@urbanelegance.com</p>
             </div>
             
-         
+         <SocialLinks />
             <p className="p-4 flex justify-center text-gray-500">Copyright 2024. All Rights Reserved</p>
         </div>
     );
